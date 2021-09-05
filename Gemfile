a0acebea42148e0d62fa3b56481eefa245519ebc
Nico Proto <nicolasproto@gmail.com>
2021-09-05 18:28:12 +0200

    Add simple Github Action workflow

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ group :development, :test do  gem 'pry-byebug'
 
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+
+  # Added by me
+  gem 'launchy'
 end
 
 group :development do
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name return the capitalized first name and last name" do
+    user = User.new(first_name: "robert", last_name: "landon")
+    assert_equal "Robert Landon", user.full_name
+  end
 end
